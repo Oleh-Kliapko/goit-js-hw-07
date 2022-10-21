2ba261492ebe0fd2b71655840f684597a8580a33
Oleh Kliapko <goldclub1977@gmail.com>
2022-10-21 15:54:05 +0300

    add lazy loading of pictures

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,14 +6,31 @@ galleryContainer.insertAdjacentHTML(
   makeGalleryMarkup(galleryItems)
 );
 
+if ("loading" in HTMLImageElement.prototype) {
+  const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+  lazyImages.forEach((img) => {
+    img.src = img.dataset.src;
+  });
+} else {
+  const script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+  script.integrity =
+    "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+  script.crossOrigin = "anonymous";
+  script.referrerPolicy = "no-referrer";
+  document.body.appendChild(script);
+}
+
 function makeGalleryMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
       <div class="gallery__item">
         <a class="gallery__link" href="${original}">
-        <img class="gallery__image"
-            src="${preview}"
+        <img class="gallery__image lazyload"
+            loading="lazy"
+            data-src="${preview}"
             data-source="${original}"
             alt="${description}"
           />
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,22 +2,12 @@ import { galleryItems } from "./gallery-items.js";
 
 const galleryContainer = document.querySelector(".gallery");
 
-// galleryContainer.insertAdjacentHTML(
-//   "beforeend",
-//   makeGalleryMarkup(galleryItems)
-// );
-
 galleryContainer.innerHTML = makeGalleryMarkup(galleryItems);
 
-function makeGalleryMarkup(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-         <a class="gallery__item" href="${original}">
-            <img class="gallery__image" src="${preview}" alt="${description}" />
-        </a>`;
-    })
-    .join("");
+if ("loading" in HTMLImageElement.prototype) {
+  addSrcElementForLazyLoading();
+} else {
+  addLazySizesScript();
 }
 
 new SimpleLightbox(".gallery a", {
@@ -29,6 +19,37 @@ const galleryContainerClick = galleryContainer.addEventListener(
   "click",
   getRightClick
 );
+
+/** functions */
+
+function addSrcElementForLazyLoading() {
+  const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+  lazyImages.forEach((img) => {
+    img.src = img.dataset.src;
+  });
+}
+
+function addLazySizesScript() {
+  const script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+  script.integrity =
+    "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+  script.crossOrigin = "anonymous";
+  script.referrerPolicy = "no-referrer";
+  document.body.appendChild(script);
+}
+
+function makeGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+         <a class="gallery__item" href="${original}">
+            <img class="gallery__image lazyload" loading="lazy" data-src="${preview}" alt="${description}" />
+        </a>`;
+    })
+    .join("");
+}
 
 function getRightClick(evt) {
   evt.preventDefault();
